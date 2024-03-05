0de518cf128695abdcb1e0d0f1184dcc073e671f
mgn901 <mgn901+dev@gmail.com>
2024-03-05 05:55:29 +0000

    fix: Workspace directory name is wrong

--- scripts/on_sigterm.sh
+++ scripts/on_sigterm.sh
@@ -3,7 +3,7 @@ set -xe
 
 IS_COMPOSE=`cat "/workspaces/$1/$2" | node /tmp/coder-devcontainer-builder/is_compose_based.js | tr -d '\n'`
 if [ "$IS_COMPOSE" = "true" ]; then
-  docker compose -p "${3}_devcontainer" down
+  docker compose -p "${1}_devcontainer" down
 else
-  docker container rm "${3}_devcontainer"
+  docker container rm "${1}_devcontainer"
 fi
